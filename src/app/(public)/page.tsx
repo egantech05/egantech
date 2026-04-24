3c97da3ab2042e8a153da8e3c98f574c90cabda3
egantech05 <eganxhart@gmail.com>
2026-04-25 11:26:15 +1200

    home page added

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,32 @@
+import HeroBlock from '@/components/blocks/HeroBlock'
+import LogoListBlock from '@/components/blocks/LogoListBlock'
+import PillsBlock from '@/components/blocks/PillsBlock'
+import CardGridBlock from '@/components/blocks/CardGridBlock'
+
 export default function Home() {
   return (
-    <main>
-      <h1>Egantech</h1>
-    </main>
+    <>
+      <HeroBlock
+        config={{
+          heading: 'Egantech',
+          subheading: 'shaping the future',
+        }}
+      />
+      <LogoListBlock
+        config={{
+          title: 'Technologies',
+        }}
+      />
+      <PillsBlock
+        config={{
+          title: 'Topics',
+        }}
+      />
+      <CardGridBlock
+        config={{
+          count: 3,
+        }}
+      />
+    </>
   )
 }
