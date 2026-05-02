c51b9a20b326bc2fa376038ea53027d848f48fb9
egantech05 <eganxhart@gmail.com>
2026-05-02 16:43:49 +1200

    bold styling for markdown generator

--- src/components/blocks/RichTextBlock.tsx
+++ src/components/blocks/RichTextBlock.tsx
@@ -14,6 +14,7 @@ export default function RichTextBlock({ config }: { config: Section['config'] })
                 max-w-none
                 prose-p:text-gray-300
                 prose-headings:text-gray-200
+                prose-strong:text-gray-100
                 ">
                     <ReactMarkdown>{content ?? ''}</ReactMarkdown>
                 </div>
